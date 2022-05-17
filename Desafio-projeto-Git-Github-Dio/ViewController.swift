24f1d68c11df3730033364657e529910bb9a44c2
Marne <marnevinicius@hotmail.com>
2022-05-17 19:59:24 -0300

    Initial Commit

---
+++ Desafio-projeto-Git-Github-Dio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Desafio-projeto-Git-Github-Dio
+//
+//  Created by Marne on 17/05/22.
+//  Copyright © 2022 Marne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Desafio-projeto-Git-Github-DioTests/Desafio_projeto_Git_Github_DioTests.swift
@@ -0,0 +1,34 @@
+//
+//  Desafio_projeto_Git_Github_DioTests.swift
+//  Desafio-projeto-Git-Github-DioTests
+//
+//  Created by Marne on 17/05/22.
+//  Copyright © 2022 Marne. All rights reserved.
+//
+
+import XCTest
+@testable import Desafio_projeto_Git_Github_Dio
+
+class Desafio_projeto_Git_Github_DioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
